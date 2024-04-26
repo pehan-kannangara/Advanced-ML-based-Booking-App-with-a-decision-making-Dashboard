89c2928f8292b26e266b61827789f96a0cafade3
Pehan Kannangara <109351698+pehan-kannangara@users.noreply.github.com>
2024-04-26 21:52:12 +0530

    Add files via upload

---
+++ gh url.txt
@@ -0,0 +1,1 @@
+https://github.com/safak/youtube2022/tree/mern-booking
---
+++ index.js
@@ -0,0 +1,49 @@
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+import usersRoute from "./routes/users.js";
+
+const app = express();
+dotenv.config();
+
+//DB Connection
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB disconnected");
+});
+
+//middlewares
+app.use(cookieParser());
+app.use(express.json()); //err handling middleware
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
+
+app.listen(8800, () => {
+  connect();
+  console.log("Connected to backend!");
+});
